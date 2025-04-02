11951e1c1adca792c02b849e052b9c85e798e275
Vidhanvyrs <vidhanvyrs@gmail.com>
2025-04-02 19:29:43 +0530

    Orders Dummy Page Built

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -114,7 +114,7 @@ const PlaceOrder = () => {
           <div className="w-full text-end mt-8">
             <button
               onClick={() => navigate("/orders")}
-              className="bg-black text-white px-16 py-3 text-sm"
+              className="bg-black cursor-pointer text-white px-16 py-3 text-sm"
             >
               PLACE ORDER
             </button>
